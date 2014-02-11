50865baaaa0df7370b00c78359aabd490bbb2bcd
Douglas G.R <gontijo.douglas@gmail.com>
2014-02-10 23:32:18 -0200

    Adds the description entry to application

--- library/Emyi/Util/Config.php
+++ library/Emyi/Util/Config.php
@@ -41,6 +41,7 @@ class Config
             'maintenance'   => false,
             'environment'   => 'development',
             'timezone'      => 'UTC',
+            'description'   => '',
 
             // Language code for this installation. All choices can be found here:
             // http://www.i18nguy.com/unicode/language-identifiers.html
